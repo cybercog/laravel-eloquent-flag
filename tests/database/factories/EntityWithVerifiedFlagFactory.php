78b48073b1c152871c3e7d31c2539fdd7bb95e37
ell <ell@cybercog.su>
2016-12-26 22:31:50 +0300

    Add verified trait

---
+++ tests/database/factories/EntityWithVerifiedFlagFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * This file is part of Laravel Eloquent Flag.
+ *
+ * (c) CyberCog <support@cybercog.su>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+$factory->define(\Cog\Flag\Tests\Stubs\Models\EntityWithVerifiedFlag::class, function (\Faker\Generator $faker) {
+    return [
+        'name' => $faker->word,
+        'is_verified' => true,
+    ];
+});
